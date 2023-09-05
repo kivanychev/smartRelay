302999888575a12db5e5bc2c4d4f942ab3a97f3c
Kirill Ivanychev <kivanych@gmail.com>
2023-09-05 20:59:13 +0300

    Added LVGL library and ADC basic functionality

--- main/component.mk
+++ main/component.mk
@@ -1,3 +1,4 @@
 #
 # Main Makefile. This is basically the same as a component makefile.
 #
+CFLAGS+= -DLV_LVGL_H_INCLUDE_SIMPLE
--- main/smartRelay.c
+++ main/smartRelay.c
@@ -17,41 +17,63 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/adc.h"
-#include "esp_adc_cal.h"
+#include "esp_freertos_hooks.h"
+#include "freertos/semphr.h"
 
+#include "driver/adc.h"
+#include "driver/gpio.h"
+
+#include "esp_adc_cal.h"
+#include "esp_system.h"
+
+
+
+#include "lvgl.h"
+#include "lvgl_helpers.h"
+
+
+#define LV_TICK_PERIOD_MS 1
 
 //ADC Channels
-#if CONFIG_IDF_TARGET_ESP32
 #define ADC1_EXAMPLE_CHAN0          ADC1_CHANNEL_6
 #define ADC2_EXAMPLE_CHAN0          ADC2_CHANNEL_0
 static const char *TAG_CH[2][10] = {{"ADC1_CH6"}, {"ADC2_CH0"}};
-#else
-#define ADC1_EXAMPLE_CHAN0          ADC1_CHANNEL_2
-#define ADC2_EXAMPLE_CHAN0          ADC2_CHANNEL_0
-static const char *TAG_CH[2][10] = {{"ADC1_CH2"}, {"ADC2_CH0"}};
-#endif
 
 //ADC Attenuation
 #define ADC_EXAMPLE_ATTEN           ADC_ATTEN_DB_11
 
 //ADC Calibration
-#if CONFIG_IDF_TARGET_ESP32
 #define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_VREF
-#elif CONFIG_IDF_TARGET_ESP32S2
-#define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_TP
-#elif CONFIG_IDF_TARGET_ESP32C3
-#define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_TP
-#elif CONFIG_IDF_TARGET_ESP32S3
-#define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_TP_FIT
-#endif
 
 
 static int adc_raw[2][10];
 static const char *TAG = "ADC SINGLE";
 
+
+/* Creates a semaphore to handle concurrent call to lvgl stuff
+ * If you wish to call *any* lvgl function from other threads/tasks
+ * you should lock on the very same semaphore! */
+SemaphoreHandle_t xGuiSemaphore;
+
+
 static esp_adc_cal_characteristics_t adc1_chars;
 static esp_adc_cal_characteristics_t adc2_chars;
+static uint16_t current_value = 0;
+static lv_obj_t * table;
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+static void lv_tick_task(void *arg);
+static void guiTask(void *pvParameter);
+static void create_demo_application(void);
+
+
+/**********************
+ *   APPLICATION MAIN
+ **********************/
+
 
 static bool adc_calibration_init(void)
 {
@@ -74,8 +96,99 @@ static bool adc_calibration_init(void)
     return cali_enable;
 }
 
-void app_main(void)
+
+static void btn_event_handler(lv_obj_t * obj, lv_event_t event)
 {
+    char str[32];
+
+    if(event == LV_EVENT_CLICKED) {
+        printf("Clicked\n");
+    }
+    else if(event == LV_EVENT_VALUE_CHANGED) {
+        printf("Toggled\n");
+    }
+
+    sprintf(str, "%d", current_value);
+    lv_table_set_cell_value(table, 1, 1, str);
+
+    current_value++;
+}
+
+
+
+static void event_handler(lv_obj_t * obj, lv_event_t event)
+{
+    if(event == LV_EVENT_CLICKED) {
+        printf("Clicked: %s\n", lv_list_get_btn_text(obj));
+    }
+}
+
+
+/**
+ * Create a button with a label and react on click event.
+ */
+static void create_demo_application(void)
+{
+    table = lv_table_create(lv_scr_act(), NULL);
+    lv_table_set_col_cnt(table, 2);
+    lv_table_set_row_cnt(table, 3);
+    lv_obj_align(table, NULL, LV_ALIGN_IN_TOP_MID, 0, 0);
+
+    /*Make the cells of the first row center aligned */
+    lv_table_set_cell_align(table, 0, 0, LV_LABEL_ALIGN_CENTER);
+    lv_table_set_cell_align(table, 0, 1, LV_LABEL_ALIGN_CENTER);
+
+    /*Align the price values to the right in the 2nd column*/
+    lv_table_set_cell_align(table, 1, 1, LV_LABEL_ALIGN_RIGHT);
+    lv_table_set_cell_align(table, 2, 1, LV_LABEL_ALIGN_RIGHT);
+
+    lv_table_set_cell_type(table, 0, 0, 2);
+    lv_table_set_cell_type(table, 0, 1, 2);
+
+
+    /*Fill the first column*/
+    lv_table_set_cell_value(table, 0, 0, "Name");
+    lv_table_set_cell_value(table, 1, 0, "Current");
+    lv_table_set_cell_value(table, 2, 0, "Fan state");
+
+    /*Fill the second column*/
+    lv_table_set_cell_value(table, 0, 1, "Value");
+    lv_table_set_cell_value(table, 1, 1, "1A");
+    lv_table_set_cell_value(table, 2, 1, "OFF");
+
+    lv_table_ext_t * ext = lv_obj_get_ext_attr(table);
+    ext->row_h[0] = 20;
+
+
+
+    // BUTTONS
+
+    lv_obj_t * label;
+
+    lv_obj_t * btn1 = lv_btn_create(lv_scr_act(), NULL);
+    lv_obj_set_event_cb(btn1, btn_event_handler);
+    lv_obj_align(btn1, NULL, LV_ALIGN_CENTER, 0, 30);
+
+    label = lv_label_create(btn1, NULL);
+    lv_label_set_text(label, "Button");
+
+    lv_obj_t * btn2 = lv_btn_create(lv_scr_act(), NULL);
+    lv_obj_set_event_cb(btn2, btn_event_handler);
+    lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 0, 90);
+    lv_btn_set_checkable(btn2, true);
+    lv_btn_toggle(btn2);
+    lv_btn_set_fit2(btn2, LV_FIT_NONE, LV_FIT_TIGHT);
+
+    label = lv_label_create(btn2, NULL);
+    lv_label_set_text(label, "Toggled");
+
+}
+
+
+
+static void guiTask(void *pvParameter) 
+{
+    char str[32];
     esp_err_t ret = ESP_OK;
     uint32_t voltage = 0;
     bool cali_enable = adc_calibration_init();
@@ -83,28 +196,120 @@ void app_main(void)
     //ADC1 config
     ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
     ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_EXAMPLE_CHAN0, ADC_EXAMPLE_ATTEN));
-    //ADC2 config
-    ESP_ERROR_CHECK(adc2_config_channel_atten(ADC2_EXAMPLE_CHAN0, ADC_EXAMPLE_ATTEN));
+
+
+
+    (void) pvParameter;
+    xGuiSemaphore = xSemaphoreCreateMutex();
+
+    lv_init();
+
+    /* Initialize SPI or I2C bus used by the drivers */
+    lvgl_driver_init();
+
+    lv_color_t* buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    assert(buf1 != NULL);
+
+
+    lv_color_t* buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    assert(buf2 != NULL);
+
+    static lv_disp_buf_t disp_buf;
+
+    uint32_t size_in_px = DISP_BUF_SIZE;
+
+
+    /* Initialize the working buffer depending on the selected display.
+     * NOTE: buf2 == NULL when using monochrome displays. */
+    lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
+
+    lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.flush_cb = disp_driver_flush;
+
+
+    disp_drv.buffer = &disp_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    /* Register an input device when enabled on the menuconfig */
+#if CONFIG_LV_TOUCH_CONTROLLER != TOUCH_CONTROLLER_NONE
+    lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.read_cb = touch_driver_read;
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    lv_indev_drv_register(&indev_drv);
+#endif
+
+    /* Create and start a periodic timer interrupt to call lv_tick_inc */
+    const esp_timer_create_args_t periodic_timer_args = {
+        .callback = &lv_tick_task,
+        .name = "periodic_gui"
+    };
+    esp_timer_handle_t periodic_timer;
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
+
+    /* Create the demo application */
+    create_demo_application();
 
     while (1) {
+        /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
+        vTaskDelay(pdMS_TO_TICKS(10));
+
+        /* Try to take the semaphore, call lvgl related function on success */
+        if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY)) {
+            lv_task_handler();
+        
+            xSemaphoreGive(xGuiSemaphore);
+       }
+
+
+
+        /* ADC actions */
+
         adc_raw[0][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN0);
         ESP_LOGI(TAG_CH[0][0], "raw  data: %d", adc_raw[0][0]);
-        if (cali_enable) {
+        //if (cali_enable) 
+        {
             voltage = esp_adc_cal_raw_to_voltage(adc_raw[0][0], &adc1_chars);
             ESP_LOGI(TAG_CH[0][0], "cali data: %d mV", voltage);
         }
-        vTaskDelay(pdMS_TO_TICKS(1000));
 
-        do {
-            ret = adc2_get_raw(ADC2_EXAMPLE_CHAN0, ADC_WIDTH_BIT_DEFAULT, &adc_raw[1][0]);
-        } while (ret == ESP_ERR_INVALID_STATE);
-        ESP_ERROR_CHECK(ret);
+        sprintf(str, "%d", voltage);
+        lv_table_set_cell_value(table, 1, 1, str);
 
-        ESP_LOGI(TAG_CH[1][0], "raw  data: %d", adc_raw[1][0]);
-        if (cali_enable) {
-            voltage = esp_adc_cal_raw_to_voltage(adc_raw[1][0], &adc2_chars);
-            ESP_LOGI(TAG_CH[1][0], "cali data: %d mV", voltage);
-        }
-        vTaskDelay(pdMS_TO_TICKS(1000));
     }
+
+    /* A task should NEVER return */
+    free(buf1);
+#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+    free(buf2);
+#endif
+    vTaskDelete(NULL);
+}
+
+
+
+
+
+
+
+
+
+void app_main(void)
+{
+
+    /* If you want to use a task to create the graphic, you NEED to create a Pinned task
+     * Otherwise there can be problem such as memory corruption and so on.
+     * NOTE: When not using Wi-Fi nor Bluetooth you can pin the guiTask to core 0 */
+    xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
+}
+
+
+
+
+static void lv_tick_task(void *arg) {
+    (void) arg;
+
+    lv_tick_inc(LV_TICK_PERIOD_MS);
 }
